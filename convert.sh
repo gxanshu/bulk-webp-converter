c0515e8c2682849754b28ab02f96ea92d838665f
Ai Anshu <68537640+aianshume@users.noreply.github.com>
2022-02-05 19:17:50 +0530

    Add: main file
    this is the main source code the this project

---
+++ convert.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# converting JPEG images
+find $1 -type f -and \( -iname "*.jpg" -o -iname "*.jpeg" \) \
+-exec bash -c '
+webp_path=$(sed 's/\.[^.]*$/.webp/' <<< "$0");
+if [ ! -f "$webp_path" ]; then 
+  cwebp -quiet -q 90 "$0" -o "$webp_path";
+fi;' {} \;
+
+# converting PNG images
+find $1 -type f -and -iname "*.png" \
+-exec bash -c '
+webp_path=$(sed 's/\.[^.]*$/.webp/' <<< "$0");
+if [ ! -f "$webp_path" ]; then 
+  cwebp -quiet -lossless "$0" -o "$webp_path";
+fi;' {} \;
+
+if [ -d "$home/Desktop/claybotik/webp" ] 
+then
+    mv *.webp ./webp
+else
+    mkdir webp
+    mv *.webp ./webp
+fi
